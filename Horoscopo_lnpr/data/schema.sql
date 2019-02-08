81f95aa261caac056237697f2b43556e50c6c401
Norberto Rodriguez <37199801+LuisNorberto1998@users.noreply.github.com>
2019-02-08 15:15:23 -0600

    Modificacion script base de datos

--- Horoscopo_lnpr/data/schema.sql
+++ Horoscopo_lnpr/data/schema.sql
@@ -3,9 +3,9 @@ CREATE DATABASE horoscopo_lnpr;
 USE horoscopo_lnpr;
 
 CREATE TABLE horoscopos(
-    id_signo INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id_signo INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nombre_signo   VARCHAR(100)    NOT NULL,
-    descripcion_signo VARCHAR     NOT NULL
+    descripcion_signo LONGTEXT     NOT NULL
 );
 
 
